f35c31649959bc30a0ad722ab734290defa16a07
tbrowder <tom.browder@gmail.com>
2016-01-09 12:09:09 -0600

    more reorg

--- NWF-Computer-Tech/computech16/my-presentations/nwflug-2016-01-04/make-beamer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# output
-O=agenda.pdf
-
-# create it
-pandoc -t beamer agenda.pdoc -o $O
-
-echo "See file '$O'"
--- NWF-Computer-Tech/computech16/my-presentations/nwflug-2016-01-04/null.p6
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env perl6
-# End program Last changed: 2016-01-01 16:30:32
