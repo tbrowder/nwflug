f18b35a4cc534b5759749faf4f51c061734d8f81
tbrowder <tom.browder@gmail.com>
2016-01-09 11:56:53 -0600

    gross changes for reorg

--- NWF-Computer-Tech/computech16/my-presentations/intro-to-linux/make-deck.sh
+++
@@ -1,6 +0,0 @@
-# --theme (default, flower, i18n, pixel, yatil)
-
-TMPLDIR=/usr/local/src/asciidoctor-deck.js/templates/haml
-
-# run it
-asciidoctor -T $TMPLDIR $1
--- NWF-Computer-Tech/computech16/my-presentations/intro-to-linux/make-reveal.sh
+++
@@ -1,4 +0,0 @@
-TMPLDIR=/usr/local/src/asciidoctor-reveal.js/templates/slim
-
-# run it
-asciidoctor -T $TMPLDIR $1
